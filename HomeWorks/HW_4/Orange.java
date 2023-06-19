b3c72c4538bef6422a2ee61a2a233d0cb0800b85
Daria Gnevina <maivery@mail.ru>
2023-06-19 17:49:06 +0300

    HW_4

---
+++ HomeWorks/HW_4/Orange.java
@@ -0,0 +1,11 @@
+package HW_4;
+
+public class Orange extends Fruit {
+    private static final String NAME = "Orange";
+    private static final double WEIGHT = 1.5;
+
+    public Orange() {
+        super(WEIGHT, NAME);
+    }
+
+}
